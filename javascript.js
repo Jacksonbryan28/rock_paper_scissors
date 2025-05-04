3fd75684bb99c16bcd8eeb75e1eb5c83dc902be5
Jackson <jacksonbryan280@gmail.com>
2025-05-04 13:22:51 -0400

    finished getComputerChoice function - working correctly

--- javascript.js
+++ javascript.js
@@ -3,18 +3,29 @@ console.log("Javascript Test");
 // psuedo code
 // write a function that gets computer getComputerChoic
 
-// inside the function
+function getComputerChoice() {
+  // inside the function
 
-// create a variable for the computer choice
+  // create a variable for the computer choice
+  let choice, mathVal;
 
-// use math random to generate a number0-1
+  // use math random to generate a number0-1
+  mathVal = Math.random();
+  console.log(mathVal);
+  // if it is .0-.33 set computer choice to "rock"
+  if (mathVal <= 0.33) {
+    choice = "rock";
+  }
+  // if it is .34-.66 set computer choice to "paper"
+  else if (mathVal > 0.33 && mathVal <= 0.66) {
+    choice = "paper";
+  }
+  // if it is .66-1 set computer choice to "scissor"
+  else if (mathVal > 0.66) {
+    choice = "scissor";
+  }
+  // return string
+  return choice;
+}
 
-// if it is .0-.33 set computer choice to "rock"
-
-// if it is .34-.66 set computer choice to "paper"
-
-// if it is .66-1 set computer choice to "scissor"
-
-// return string
-
-function getComputerChoice() {}
+console.log(getComputerChoice());
