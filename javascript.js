852269b5f8080965d42340dd6722e06c75812183
Jackson <jacksonbryan280@gmail.com>
2025-05-04 13:13:16 -0400

    added psuedo code in comments of getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,20 @@
 console.log("Javascript Test");
+
+// psuedo code
+// write a function that gets computer getComputerChoic
+
+// inside the function
+
+// create a variable for the computer choice
+
+// use math random to generate a number0-1
+
+// if it is .0-.33 set computer choice to "rock"
+
+// if it is .34-.66 set computer choice to "paper"
+
+// if it is .66-1 set computer choice to "scissor"
+
+// return string
+
+function getComputerChoice() {}
